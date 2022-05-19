aa3699360c592f3fe79e809c6906bc0caee7a175
xuejp <1663699296@qq.com>
2022-05-19 09:58:37 +0800

    最终

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,14 +1,10 @@
 <script setup>
-import img1 from "../../assets/img/1.jpeg";
-import img2 from "../../assets/img/2.jpeg";
-import img3 from "../../assets/img/3.jpeg";
-import Icon from "../../components/Icon/index.vue";
 import homeSwitch from "../../composition/useHomeSwitch.js";
 
 const data = [
-  { img: img1, title: "凛冬将至", desc: "人唯有恐惧的时候方能勇敢" },
-  { img: img2, title: "血火同源", desc: "如果我回头，一切都完了" },
-  { img: img3, title: "听我怒吼", desc: "兰尼斯特有债必偿" },
+  { img: 'https://pic.rmb.bdstatic.com/bjh/07e924164fc44ff219328c4ad78b032e.jpeg', title: "凛冬将至", desc: "人唯有恐惧的时候方能勇敢" },
+  { img: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2019-11-08%2F5dc528500579f.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1655473678&t=13704eae83cab748cc754c57098f6a23', title: "血火同源", desc: "如果我回头，一切都完了" },
+  { img: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2020-06-12%2F5ee34f1807833.jpg&refer=http%3A%2F%2Fpic1.win4000.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1655473602&t=69b91f7f6b9b40bb1325cd3b195722a7', title: "听我怒吼", desc: "兰尼斯特有债必偿" },
 ];
 
 const {
